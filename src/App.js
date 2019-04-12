84e2987e20b38c008dc923f1c6c0be5271dfc4b1
nunna19 <nunnapat_ch@yahoo.com>
2019-04-11 22:21:32 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+library.add(faStroopwafel)
+
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      <header className="Navbar">
+        <img className="logo" src="./img/DarkBG.svg"/>
+        <FontAwesomeIcon className="icon" icon={faBars} />
+      </header>
+
       </div>
     );
   }
---
+++ src/Components/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../App.scss';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+
+      
+        <main className="main">
+          <img className="mainImg"src="./img/hero-image.jpg" />
+        </main>
+      
+
+      </div>
+    );
+  }
+}
+
+export default Home;
